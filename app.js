d972bc5b4c7dd2d8e6aa7b96670da470abbb4ac9
hicommission <37531397+hicommission@users.noreply.github.com>
2018-03-25 03:50:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,77 @@
-//$(document).ready(function() {
-    // model
-    var Movie = Backbone.Model.extend({
-        defaults: function() {
-            return {
-                name: ""
-            };
+var favMovies = new Firebase('https://moviefire.firebaseio.com/movies');
+
+function saveToList(event) {
+    if (event.which == 13 || event.keyCode == 13) { // as the user presses the enter key, we will attempt to save the data
+        var movieName = document.getElementById('movieName').value.trim();
+        if (movieName.length > 0) {
+            saveToFB(movieName);
         }
+        document.getElementById('movieName').value = '';
+        return false;
+    }
+};
+
+function saveToFB(movieName) {
+    // this will save data to Firebase
+    favMovies.push({
+        name: movieName
     });
-    //collection
-    var Movies = Backbone.Firebase.Collection.extend({
-        model: Movie,
-        firebase: new Firebase("https://moviefire.firebaseio.com/movies")
-    });
-    // init collection
-    var favMovies = new Movies();
-    //view
-    var favMovieView = Backbone.View.extend({
-        tagName: "li",
-        events: {
-            "click .delete": "clear",
-            "click .edit": "edit",
-        },
-        initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
-            this.listenTo(this.model, 'remove', this.remove);
-        },
-        render: function() {
-            //this.$el.html(_.template($('#movieTemplate').html().trim(), this.model.toJSON()));
-            this.$el.html(_.template($('#movieTemplate#addMeTemplate').html().trim(), this.model.toJSON()));
-            return this;
-        },
-        edit: function() {
-            var movieName = prompt("Update the movie name", this.model.get('name').trim()); // to keep things simple and old skool :D
-            if (movieName && movieName.length > 0) {
-                this.model.set({
-                    name: movieName
-                });
-            }
-        },
-        clear: function() {
-            var response = confirm("Are certain about removing \"" + this.model.get('name').trim() + "\" from the list?");
-            if (response == true) {
-                favMovies.remove(this.model);
+};
+
+function refreshUI(list) {
+    var lis = '';
+    for (var i = 0; i < list.length; i++) {
+        lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' [' + genLinks(list[i].key, list[i].name) + ']</li>';
+    };
+    document.getElementById('favMovies').innerHTML = lis;
+};
+
+function genLinks(key, mvName) {
+    var links = '';
+    links += '<a href="javascript:edit(\'' + key + '\',\'' + mvName + '\')">Edit</a> | ';
+    links += '<a href="javascript:del(\'' + key + '\',\'' + mvName + '\')">Delete</a>';
+    return links;
+};
+
+function edit(key, mvName) {
+    var movieName = prompt("Update the movie name", mvName); // to keep things simple and old skool :D
+    if (movieName && movieName.length > 0) {
+        // build the FB endpoint to the item in movies collection
+        var updateMovieRef = buildEndPoint(key);
+        updateMovieRef.update({
+            name: movieName
+        });
+    }
+}
+
+function del(key, mvName) {
+    var response = confirm("Are certain about removing \"" + mvName + "\" from the list?");
+    if (response == true) {
+        // build the FB endpoint to the item in movies collection
+        var deleteMovieRef = buildEndPoint(key);
+        deleteMovieRef.remove();
+    }
+}
+
+function buildEndPoint (key) {
+	return new Firebase('https://moviefire.firebaseio.com/movies/' + key);
+}
+
+// this will get fired on inital load as well as when ever there is a change in the data
+favMovies.on("value", function(snapshot) {
+    var data = snapshot.val();
+    var list = [];
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            name = data[key].name ? data[key].name : '';
+            if (name.trim().length > 0) {
+                list.push({
+                    name: name,
+                    key: key
+                })
             }
         }
-    });
-//alert('testimas');
-/*
-    var AppView = Backbone.View.extend({
-        el: $("body"),
-        //el: $("favMovie"),
-        events: {
-            "keypress #movieName": "saveToList"
-        },
-        initialize: function() {
-            this.input = this.$("#movieName");
-            this.listenTo(favMovies, 'add', this.addOne);
-        },
-        saveToList: function(e) {
-            if (e.keyCode != 13) return
-            else {
-                if (this.input.val().length > 0)
-                    favMovies.add({
-                        name: this.input.val()
-                    });
-            }
-            this.input.val('');
-        },
-        addOne: function(movie) {
-            if ($('#loading').length > 0) $('#loading').remove();
-            var view = new favMovieView({
-                model: movie
-            });
-            this.$("#favMovies").append(view.render().el);
-        }
-    });
-    var App = new AppView();
-     */
-//});
+    }
+    // refresh the UI
+    refreshUI(list);
+});
