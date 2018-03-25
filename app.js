b4863f22d78011b7c40cff91a4a27d4864be91c8
hicommission <37531397+hicommission@users.noreply.github.com>
2018-03-25 02:56:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //$(document).ready(function() {
     // model
- /*   var Movie = Backbone.Model.extend({
+    var Movie = Backbone.Model.extend({
         defaults: function() {
             return {
                 name: ""
@@ -75,5 +75,5 @@
         }
     });
     var App = new AppView();
-    */
+    /* */
 //});
